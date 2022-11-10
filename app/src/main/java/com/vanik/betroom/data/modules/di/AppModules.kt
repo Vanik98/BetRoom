0c5eed16f9db310bf01967c86029efc0d107b90e
vanikdallakyan <dallakyanvanik@gmail.com>
2022-11-10 12:39:22 +0400

    change directory structure

--- app/src/main/java/com/vanik/betroom/data/modules/di/AppModules.kt
+++ app/src/main/java/com/vanik/betroom/data/modules/di/AppModules.kt
@@ -24,30 +24,30 @@ val appModules by lazy {
     )
 }
 
-val viewModelModule = module {
+private val viewModelModule = module {
     viewModel { MainViewModel(get(), get()) }
     viewModel { MovieViewModel(get(), get()) }
 }
 
-val actorUseCaseModule = module {
+private val actorUseCaseModule = module {
     single { AddActorUseCase(get()) }
     single { GetActorUseCase(get()) }
 }
 
-val movieUseCaseModule = module {
+private val movieUseCaseModule = module {
     single { AddMovieUseCase(get()) }
     single { GetMovieUseCase(get()) }
 }
 
-val repositoryModule = module { single { Repository(get(), get(), get()) } }
+private val repositoryModule = module { single { Repository(get(), get(), get()) } }
 
-val roomModule = module {
+private val roomModule = module {
     single { Room.databaseBuilder(get(), AppDatabase::class.java, "database_room").build() }
     single { get<AppDatabase>().ActorDao() }
     single { get<AppDatabase>().MovieDao() }
 }
 
-val sqlLiteModule = module { single { DBHelper(get()) } }
+private val sqlLiteModule = module { single { DBHelper(get()) } }
 
 
 
